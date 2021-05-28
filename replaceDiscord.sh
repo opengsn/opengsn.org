689cdc264067002e3a96cb5bef9976d64ce96e40
Dror Tirosh <dror@opengsn.org>
2021-05-28 14:33:34 +0300

    replace discord invite link, add script to replace discord.

---
+++ replaceDiscord.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -xe 
+#note: we don't replace the https prefix, since some occurences have the protocol escaped
+newlink='discord.gg/sUGsbUM5'
+
+#verify link is valid:
+if ! curl -s -L https://$newlink|grep -m 1 OpenGSN; then
+   echo "FATAL: broken new link"
+   exit 2
+fi
+
+oldlink=`perl -ne 'print "$1\n" if /(discord.gg.\w+)/' snippets/1.html |head -1`
+
+#old (stale) link doesn't have the channel name...
+if curl -s -L https://$oldlink|grep -m 1 OpenGSN; then
+   echo "WARNING: replacing a good (https://$oldlink) link."
+fi
+
+perl -pi -e "s!discord.gg/\w+!$newlink!g" `grep -r -l discord . --include '*.html'`
